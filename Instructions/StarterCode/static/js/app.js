4a76b45934d34408ae4f8408705fe276c6a7139b
busy0312 <abbymacbook@Abbys-MacBook-Pro.local>
2020-05-18 03:47:41 -0500

    edited

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,5 +1,5 @@
 
-// //complete dropdown list
+//complete dropdown list
 (async function testid(){
   var data =await d3.json("samples.json");
   console.log(data)
@@ -8,14 +8,15 @@
       var idtag=document.createElement('option');
       idtag.textContent=JSON.stringify(x);
       document.querySelector('#selDataset').appendChild(idtag)
-      getdata()
     })})()
+
 
 
 //catch dropdown change
 d3.selectAll("#selDataset").on("change", getdata);
 //updatedata
 async function getdata(){
+
   var dropdown=d3.selectAll('option');
   var ids=dropdown.property('value');
   
@@ -23,15 +24,18 @@ async function getdata(){
   var samplesdata=data.samples
   var id=samplesdata.map(d=>d)
   var newid=id.filter(d=>d.id===ids)
-  
   newid.forEach(function(x){
     var indi_id=x.id
     var otu=x.otu_ids
     var sampleValues = x.sample_values;
     var labels = x.otu_labels;
-    var new_otu = otu.map(d=>`OTU ${d}`)
+    
+   //
+  //  function OTUfunction(num){
+  //   return `OTU ${num}`
+  //   }
+var new_otu = otu.map(d=>`OTU ${d}`)
 
-//create a bar chart
 var trace1 = {
 x: sampleValues.slice(0,9).reverse(),
 y:new_otu.slice(0,9).reverse(),
@@ -41,9 +45,9 @@ y:new_otu.slice(0,9).reverse(),
   text:labels.slice(0,9).reverse()
 };
 var data_bar = [trace1];
+// Plot the chart to a div tag with id "plot"
 Plotly.newPlot("bar", data_bar);
 
-//create the bubble chart
 var trace2 = {
 x: otu,
 y: sampleValues,
@@ -54,46 +58,42 @@ size: sampleValues,
 color:otu,
 }
 };
+
 var data_bubble = [trace2];
+
+
 Plotly.newPlot('bubble', data_bubble);
 
     
 })
 }
 
-// the function to get individual information
 
 
-async function getinfo(ids){
-  var data =await d3.json("samples.json");
-  var metadata=data.metadata;
-  var new_metadata=metadata.map(d=>d);
-  var new_meta_id=new_metadata.filter(d=>d.id===ids);
-  new_meta_id.forEach(function(info){
-    var ids=info.id
-    var ethnicity=info.ethnicity;
-    var gender=info.gender;
-    var age=info.age;
-    var location=info.location;
-    var bbtype=info.bbtype;
-    var wfreq=info.wfreq
-    var demo_info=d3.select('#sample-metadata')
-    demo_info.append('p').text(`id: ${ids}`)
-    demo_info.append('p').text(`ethnicity: ${ethnicity}`)
-    demo_info.append('p').text(`gender: ${gender}`)
-    demo_info.append('p').text(`age: ${age}`)
-    demo_info.append('p').text(`location: ${location}`)
-    demo_info.append('p').text(`bbtype: ${bbtype}`)
-    demo_info.append('p').text(`wfreq: ${wfreq}`)
+
+
+
+
+//     var ids=data.metadata[0].id
+//     var ethnicity=data.metadata[0].ethnicity;
+//     var gender=data.metadata[0].gender;
+//     var age=data.metadata[0].age;
+//     var location=data.metadata[0].location;
+//     var bbtype=data.metadata[0].bbtype;
+//     var wfreq=data.metadata[0].wfreq;
+//     var info=d3.select('#sample-metadata')
+
+//     info.append('p').text(`id: ${ids}`)
+//     info.append('p').text(`ethnicity: ${ethnicity}`)
+//     info.append('p').text(`gender: ${gender}`)
+//     info.append('p').text(`age: ${age}`)
+//     info.append('p').text(`location: ${location}`)
+//     info.append('p').text(`bbtype: ${bbtype}`)
+//     info.append('p').text(`wfreq: ${wfreq}`)
     
-  })
-}
 
 
 
 
-
-
-
-
+// })()
 
